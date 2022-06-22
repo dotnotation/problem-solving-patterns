6fc9fa5b7557c6133dce55c63837247ad8593ecd
dotnotation <dotinmotionproductions@gmail.com>
2022-06-22 12:53:05 -0400

    added prompt and notes for validAnagram function

---
+++ valid_anagram.js
@@ -0,0 +1,10 @@
+// Given two strings
+// write a function to determine if the second string is an anagram of the first
+// all inputs are single words with no special characters and are lowercase
+// ex. anagram("anagram", "nagaram") = true
+// ex. anagram("rat", "car") = false
+
+function validAnagram(){
+    // create objects for each string
+    // compare the objects
+}
