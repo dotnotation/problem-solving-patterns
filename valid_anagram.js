7795337dc69d2748bfa0ec3b5048345976d91c73
dotnotation <dotinmotionproductions@gmail.com>
2022-06-22 15:00:16 -0400

    added Colt Steele's solution and notes

--- valid_anagram.js
+++ valid_anagram.js
@@ -35,3 +35,33 @@ function validAnagram(str1, str2){
 
 validAnagram("rat", "car")
 validAnagram("anagram", "nagaram")
+
+// Colt Steele's Solution
+function validAnagram(first, second){
+    if (first.length !== second.length){
+        return false
+    }
+
+    const lookup = {}
+
+    for (let i = 0; i < first.length; i++){
+        let letter = first[i]
+        // if letter exists, increment, otherwise set to 1
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1
+        // does the lookup object already have the letter in it?
+        // if the letter is already there, add 1
+        // if the letter isn't there, assign it a value of 1
+    }
+
+    for (let i = 0; i < second.length; i++){
+        let letter = second[i]
+        // can't find letter or letter is zero then it's not an anagram
+        if (!lookup[letter]){
+            return false
+        } else {
+            lookup[letter] -= 1
+            // if all the letters exist then by the end all the values of the keys will be 0 meaning that we found all the letters in the second string
+        }
+    }
+    return true
+}
