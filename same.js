2ecdb3a9c9047a09daa8edf8c2686f52fb88f935
dotnotation <dotinmotionproductions@gmail.com>
2022-06-22 10:14:56 -0400

    added first solution to same function

--- same.js
+++ same.js
@@ -2,6 +2,21 @@
 // accepts two arrays
 // should return true if every value in the array has a value that is the squared of that value in the second array
 // the frequency of values must be the same
-// ex. same([1,2,3], [4,1,9]) = true
+// ex. same([1,2,3], [4,1,9]) = true (although the order is mixed up it is ok)
 // ex. same([1,2,3], [1,9]) = false
-// ex. same([1,2,1], [4,4,1]) = false (must be same frequency)
+// ex. same([1,2,1], [4,4,1]) = false (must be same frequency because we need 1 in the second array twice and 4 only once)
+
+function same(arr1, arr2){
+    if (arr1.length !== arr2.length){
+        return false
+    }
+    for (let i = 0; i < arr1.length; i++){
+        let correctIndex = arr2.indexOf(arr1[i] ** 2)
+        if (correctIndex === -1) {
+            return false
+        }
+        arr2.splice(correctIndex, 1)
+    }
+    return true
+}
+// O(n^2)
