f9dec6080975c407e2f9feb3a7ea98187db7bf5c
dotnotation <dotinmotionproductions@gmail.com>
2022-06-22 15:21:01 -0400

    added first solution to sumZero function with notes

---
+++ sum_zero.js
@@ -0,0 +1,13 @@
+// Write a function named sumZero
+// takes in an array
+// the array contains sorted integers
+// the function should find the first pair where the sum is 0
+// returns an array that includes both values that sum to zero or undefined if a pair doesn't exist
+// ex. sumZero([-3, -2, -1, 0, 1, 2, 3]) = [-3, 3]
+// ex. sumZero([-2, 0, 1, 3]) = undefined
+
+function sumZero(arr){
+
+}
+
+sumZero([-3, -2, -1, 0, 1, 2, 3])
