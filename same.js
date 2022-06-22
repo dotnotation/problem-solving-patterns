0008e0dff5c94142234df13df7b38d3fa9905c4a
dotnotation <dotinmotionproductions@gmail.com>
2022-06-22 10:50:23 -0400

    added notes

--- same.js
+++ same.js
@@ -11,12 +11,17 @@ function same(arr1, arr2){
         return false
     }
     for (let i = 0; i < arr1.length; i++){
-        let correctIndex = arr2.indexOf(arr1[i] ** 2)
-        if (correctIndex === -1) {
+        let correctIndex = arr2.indexOf(arr1[i] ** 2) // indexOf does loop which is causing a nested loop
+        if (correctIndex === -1) { // -1 means that the index is not in the array
             return false
         }
-        arr2.splice(correctIndex, 1)
+        console.log(arr2) // as the loop goes, arr 2 will become [9,4,4], then [9,4], then [4] and then will return true
+        arr2.splice(correctIndex, 1) // if the index is in the array which returns 1, then we want to splice a that correct index which removes it and if we never return false and all the values of the second array are gone then we return true
     }
     return true
 }
 // O(n^2)
+same([1,2,3,2], [9,1,4,4])
+
+// Refactor:
+// Fix nested loop
