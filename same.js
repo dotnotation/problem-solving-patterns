4d6cca5450ca826a49ac39ef578a5f3afbb86c3a
dotnotation <dotinmotionproductions@gmail.com>
2022-06-22 09:50:02 -0400

    added notes about the prompt for the Same funciton

---
+++ same.js
@@ -0,0 +1,7 @@
+// Write a function called same
+// accepts two arrays
+// should return true if every value in the array has a value that is the squared of that value in the second array
+// the frequency of values must be the same
+// ex. same([1,2,3], [4,1,9]) = true
+// ex. same([1,2,3], [1,9]) = false
+// ex. same([1,2,1], [4,4,1]) = false (must be same frequency)
