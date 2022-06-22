10bf2468f2bad1588bf78a49f7ea21073c30f9c6
dotnotation <dotinmotionproductions@gmail.com>
2022-06-22 12:09:36 -0400

    added notes

--- same.js
+++ same.js
@@ -25,26 +25,41 @@ same([1,2,3,2], [9,1,4,4])
 
 // Refactor:
 // Fix nested loop
+// loop over each array one time individually 
+// 2 loops are better than a nested loop
 
 function same(arr1, arr2){
     if (arr1.length !== arr2.length){
+        // if the length of the arrays are not the same return false
         return false
     }
-    let frequencyCounter1 = {}
+    let frequencyCounter1 = {} 
+    // each object is going to count the frequency of the individual values in the array
+    // ex. {1: 1, 2: 2, 3: 1}
+    // so we have a key value pair showing the frequency of each number in the array
     let frequencyCounter2 = {}
     for (let val of arr1){
+        // for of is a great way to loop over a string or array
         frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+        // for each element in the array we are going to set a key and the value is either going to be initialized or 1 will be added to the value
     }
     for (let val of arr2){
         frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
     }
+    console.log(frequencyCounter1)
+    console.log(frequencyCounter2)
+
     for (let key in frequencyCounter1){
+        // for each key in the object
         if (!(key ** 2 in frequencyCounter2)){
+            // !(is the key squared from frequency1 a key in frequency2?)
             return false
         }
         if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]){
+            // do the values correspond?
             return false
         }
     }
     return true
 }
+// O(n)
