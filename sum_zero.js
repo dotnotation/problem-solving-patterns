990dd4d0f3c6f5bec7e4c06bac620a0ce700da89
dotnotation <dotinmotionproductions@gmail.com>
2022-06-22 20:59:57 -0400

    added notes

--- sum_zero.js
+++ sum_zero.js
@@ -7,7 +7,18 @@
 // ex. sumZero([-2, 0, 1, 3]) = undefined
 
 function sumZero(arr){
-
+    for (let i = 0; i < arr.length; i++){
+        for (let j = i + 1; j < arr.length; j++){
+            if (arr[i] + arr[j] === 0){
+                return [arr[i], arr[j]]
+            }
+        }
+    }
 }
+// time: O(n^2)
+// space: O(1)
 
 sumZero([-3, -2, -1, 0, 1, 2, 3])
+
+// Refactor:
+// need to get rid of nested loop
