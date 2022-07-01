258a666db5d7c52b39339d5042ee8aebac9beaa0
dotnotation <dotinmotionproductions@gmail.com>
2022-07-01 14:35:48 -0400

    added notes for the maxSubarraySum function

---
+++ max_subarray_sum.js
@@ -0,0 +1,8 @@
+// write a function called maxSubarraySum
+// takes in an array of integers and a number call n
+// the function should calculate the maximum sum of n consecutive elements in the array
+// ex. maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2) = 10
+// maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4) = 17
+// maxSubarraySum([4, 2, 1, 6], 1) = 6
+// maxSubarraySum([4, 2, 1, 6, 2], 4) = 13 
+// maxSubarraySum([], 4) = null
