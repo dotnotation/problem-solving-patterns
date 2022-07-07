87ba2374e5d61968847809e14ce4de536acb94d1
dotnotation <dotinmotionproductions@gmail.com>
2022-07-07 14:03:24 -0400

    added solution to maxSubarraySum function

--- max_subarray_sum.js
+++ max_subarray_sum.js
@@ -8,3 +8,22 @@
 // maxSubarraySum([4*, 2*, 1*, 6*, 2], 4) = 13 
 // maxSubarraySum([], 4) = null
 // * denotes the elements with the highest sum
+
+function maxSubarraySum(arr, num){
+    if (num > arr.length){
+        return null
+    }
+
+    var max = -Infinity
+
+    for (let i = 0; i < arr.length - num + 1; i++){
+        temp = 0
+        for (let j = 0; j < num; j++){
+            temp += arr[i + j]
+        }
+        if (temp > max){
+            max = temp
+        }
+    }
+    return max
+}
