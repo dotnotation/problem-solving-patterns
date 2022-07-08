21fddcd3291cfa484dbff8db14144ec4e3efe007
dotnotation <dotinmotionproductions@gmail.com>
2022-07-08 13:50:30 -0400

    added notes about sameFrequency function

---
+++ same_frequency.js
@@ -0,0 +1,14 @@
+// takes in two positive integers
+// find if the two integers have the same frequency of digits
+// must be O(n)
+// ex. sameFrequency(182, 281) = true
+// sameFrequency(34, 14) = false
+// sameFrequency(3589578, 5879385) = true
+// sameFrequency(22, 222) = false
+
+function sameFrequency(n1, n2){
+    // need to compare each number in n1 to see if all of the same numbers and amount of each number is the same as in n2
+    // loop through the n1 and remove all the same numbers from n2, if the number is 0 then return true otherwise return false
+
+    
+}
