9d75c0e2f4d75eab89e691fd92b2207326f66602
dotnotation <dotinmotionproductions@gmail.com>
2022-07-08 12:40:58 -0400

    added notes and refactored

--- max_subarray_sum.js
+++ max_subarray_sum.js
@@ -39,3 +39,20 @@ function maxSubarraySum(arr, num){
 // this isn't an efficient way especially for large arrays or large num
 // O(n^2)
 
+// Refactor:
+
+function maxSubarraySum(arr, num) {
+    let maxSum = 0
+    let tempSum = 0
+
+    if (arr.length < num) return null
+
+    for (let i = 0; i < arr.length; i++){
+        tempSum = tempSum - arr[i - num] + arr[i]
+        maxSum = Math.max(maxSum, tempSum)
+    }
+
+    return maxSum
+}
+
+// Time: 0(n)
