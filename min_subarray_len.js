077d9a252998dc6d996f390d0a40ed5ed495a779
dotnotation <dotinmotionproductions@gmail.com>
2022-07-14 16:22:42 -0400

    added notes about minSubArrayLen function

---
+++ min_subarray_len.js
@@ -0,0 +1,17 @@
+// Sliding Window
+
+// takes in an array and an integer
+// both inputs are positive 
+// return the minimal *length* of a contiguous subarray
+// where the sum is greater than or equal to the integer
+// if there is none, return 0
+// ex. minSubArrayLen([2, 3, 1, 2, 4*, 3*], 7) = 2
+// minSubArrayLen([2, 1, 6, 5*, 4*], 9) = 2
+// minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52) = 1 because 62 is greater than 52
+// minSubArrayLen([1, 4, 16*, 22*, 5*, 7*, 8*, 9, 10], 55) = 5
+// minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95) = 0
+// * denotes which subarray is used
+
+function minSubArrayLen(arr, num){
+
+}
