659ae5d1dfd0e92db311d4258080dbdd83afc6ae
dotnotation <dotinmotionproductions@gmail.com>
2022-07-14 17:22:48 -0400

    added notes for findLongestSubstring function

---
+++ find_longest_substring.js
@@ -0,0 +1,17 @@
+// Sliding Window
+
+// takes in a string
+// returns the length of the longest substring of distinct characters
+// if string is empty, return 0
+// the string doesn't have breaks or unique characters
+// string is all lower case 
+// ex. findLongestSubstring('') = 0
+// findLongestSubstring('rithmsc*hool') = 7
+// findLongestSubstring('thisis*awesom*e') = 6
+// findLongestSubstring('b*bbbbb') = 1
+// findLongestSubstring('thecat*intheha*t') = 7
+// * denotes the break in the distinct characters
+
+function findLongestSubstring(str){
+    
+}
