8a11d48b6cdcee0253efb3774d8ef5894f767bef
dotnotation <dotinmotionproductions@gmail.com>
2022-07-14 17:10:48 -0400

    added correct solution for minSubArrayLen function

--- min_subarray_len.js
+++ min_subarray_len.js
@@ -12,17 +12,31 @@
 // minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95) = 0
 // * denotes which subarray is used
 
-function minSubArrayLen(arr, num){
-    let total = 0
-    let subArray = []
-
-    for (let i = 0; i < num; i++){
-        total += arr[i]
-        if (total <= num){
-            subArray.push(i)
-        }
+function minSubArrayLen(arr, sum) {
+    let total = 0;
+    let start = 0;
+    let end = 0;
+    let minLen = Infinity;
+   
+    while (start < arr.length) {
+      // if current window doesn't add up to the given sum then 
+          // move the window to right
+      if(total < sum && end < arr.length){
+        total += arr[end];
+              end++;
+      }
+      // if current window adds up to at least the sum given then
+          // we can shrink the window 
+      else if(total >= sum){
+        minLen = Math.min(minLen, end-start);
+              total -= arr[start];
+              start++;
+      } 
+      // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
+      else {
+        break;
+      }
     }
-
-    return subArray.length
-
-}
+   
+    return minLen === Infinity ? 0 : minLen;
+  }
