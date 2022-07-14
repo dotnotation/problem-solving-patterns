5dbc46aae9e5fc4420f089b70cd98a9d65331230
dotnotation <dotinmotionproductions@gmail.com>
2022-07-14 14:22:37 -0400

    added Colt's solution to averagePair funciton

--- average_pair.js
+++ average_pair.js
@@ -35,3 +35,15 @@ function averagePair(arr, target){
     
 }
 
+// Colt's Solution
+function averagePair(arr, num){
+    let start = 0
+    let end = arr.length-1;
+    while(start < end){
+      let avg = (arr[start]+arr[end]) / 2 
+      if(avg === num) return true;
+      else if(avg < num) start++
+      else end--
+    }
+    return false;
+}
