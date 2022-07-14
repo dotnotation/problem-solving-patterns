4ed2e3f5b76deca7e524433475fce39645a5f297
dotnotation <dotinmotionproductions@gmail.com>
2022-07-14 15:11:04 -0400

    added my solution to isSubsequence function

--- is_subsequence.js
+++ is_subsequence.js
@@ -9,6 +9,25 @@
 // isSubsequence('abc', 'abracadabra') = true
 // isSubsequence('abc', 'acb') = false
 
-function isSubsequence(str1, str2){
-
-}
+function isSubsequence(str1, str2) {
+    const compare = {}
+    
+    for (let i = 0; i < str2.length; i++){
+        let letter = str2[i]
+        
+        compare[letter] ? compare[letter] += 1 : compare[letter] = 1
+    }
+    
+    for (let i = 0; i < str1.length; i++){
+        let letter = str1[i]
+        console.log(compare)
+        
+        if (!compare[letter]){
+            return false
+        } else {
+            compare[letter] -= 1
+        }
+    }
+    
+    return true
+  }
