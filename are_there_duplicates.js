3e0ae44eccdb37d18ddce87e0d30a8a17d55cbfa
dotnotation <dotinmotionproductions@gmail.com>
2022-07-14 10:59:37 -0400

    added areThereDuplicates requirements

---
+++ are_there_duplicates.js
@@ -0,0 +1,14 @@
+// function called areThereDuplicates
+// accepts a variable number of arguments
+// check to see if there are duplicates in the arguments
+// use the frequency counter pattern or multiple pointers pattern
+// the arguments could be integers or strings 
+// ex. areThereDuplicates(1, 2, 3) = false
+// areThereDuplicates(1, 2, 2) = true
+// areThereDuplicates('a', 'b', 'c', 'a') = true
+// needs to be Time: O(n) Space: O(n)
+// Bonus: Time: O(n log n) Space: O(1)
+
+function areThereDuplicates(arg){
+    
+}
