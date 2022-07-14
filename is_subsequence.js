65c8278b554b208d894119ac3b58699637d52a32
dotnotation <dotinmotionproductions@gmail.com>
2022-07-14 14:37:20 -0400

    added prompt and notes for isSubsequence function

---
+++ is_subsequence.js
@@ -0,0 +1,12 @@
+// takes in two strings
+// check to see if the characters in the first string form a subsequence of the second string
+// see if the characters in the first string appear anywhere in the second string in the same order
+// Time: O(N + M) Space: O(1)
+// ex. isSubsequence('hello', 'hello world') = true
+// isSubsequence('sing', 'string') = true
+// isSubsequence('abc', 'abracadabra') = true
+// isSubsequence('abc', 'acb') = false
+
+function isSubsequence(str1, str2){
+
+}
